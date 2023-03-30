0f5b6e2e4d5bbbfdae04fc922cb3ed4920030376
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2023-03-30 11:25:50 +1100

    A simple fetch demo using Axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
-import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
+interface User {
+  id: number;
+  name: string;
+}
 function App() {
-  return <div>Connecting to Backend using Axios</div>;
+  const [data, setData] = useState<User[]>([]);
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then(({ data }) => setData(data[0].id))
+      .catch((err) => console.log(err));
+  }, []);
+  return (
+    <>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </>
+  );
 }
 
 export default App;
