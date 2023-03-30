7de96ae0a04f27605f23c4236e7362a11c7579b0
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2023-03-30 14:21:02 +1100

    Show a loading indicator while fetching data from the server

--- src/App.tsx
+++ src/App.tsx
@@ -8,22 +8,29 @@ interface User {
 function App() {
   const [data, setData] = useState<User[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setisLoading] = useState(false);
   useEffect(() => {
     const controller = new AbortController();
+    setisLoading(true);
     axios
       .get<User[]>("https://jsonplaceholder.typicode.com/users", {
         signal: controller.signal,
       })
-      .then(({ data }) => setData(data))
+      .then(({ data }) => {
+        setisLoading(false);
+        setData(data);
+      })
       .catch((err) => {
         if (err instanceof CanceledError) return;
         setError(err.message);
+        setisLoading(false);
       });
     return () => controller.abort();
   }, []);
   return (
     <>
       {error && <p>{error}</p>}
+      {isLoading && <p>Loading...</p>}
       <ul>
         {data.map((d) => (
           <li key={d.id}>{d.name}</li>
